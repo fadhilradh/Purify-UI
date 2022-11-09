c0451a64d07df63332568de095598e0de8f3feef
Fadhil Radhian <fadhil.radhian@wartek.belajar.id>
2022-11-10 04:55:57 +0700

    add tabs functionality

--- tabs/js/main.js
+++ tabs/js/main.js
@@ -1,1 +1,14 @@
-// Start writing JavaScript here!
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (event) => {
+    tabs.forEach((tabby) => {
+      tabby.classList.remove("is-selected");
+    });
+    tab.classList.add("is-selected");
+    tabContents.forEach((t) => t.classList.remove("is-selected"));
+    const targetTabContent = document.querySelector("#" + tab.dataset.target);
+    targetTabContent.classList.add("is-selected");
+  });
+});
