84921b8c7108f6c249c4febfa916e2f38b7ec2a1
Fadhil Radhian <fadhil.radhian@wartek.belajar.id>
2022-11-12 17:58:54 +0700

    add dots and refactor

--- carousel/js/main.js
+++ carousel/js/main.js
@@ -1,31 +1,58 @@
 const prevButton = document.querySelector(".prev-button");
 const nextButton = document.querySelector(".next-button");
 const carousel = document.querySelector(".carousel__contents");
+const carouselDots = document.querySelectorAll(".carousel__dot");
+const dotsContainer = document.querySelector(".carousel__dots");
+
+function updateActiveDot(buttonType) {
+  const selectedDot = dotsContainer.querySelector(".is-selected");
+  const targetDot =
+    buttonType === "next"
+      ? selectedDot.nextElementSibling
+      : selectedDot.previousElementSibling;
+  selectedDot.classList.remove("is-selected");
+  targetDot.classList.add("is-selected");
+}
+
+function updateSlide(targetSlide, currentSlide) {
+  const targetSlideLeft = getComputedStyle(targetSlide).left;
+  carousel.style.left = "-" + targetSlideLeft;
+  currentSlide.classList.remove("is-selected");
+  targetSlide.classList.add("is-selected");
+}
 
 nextButton.addEventListener("click", () => {
   prevButton.removeAttribute("hidden");
   const currentSlide = carousel.querySelector(".is-selected");
   const nextSlide = currentSlide.nextElementSibling;
-  const nextSlideLeft = getComputedStyle(nextSlide).left;
-  carousel.style.left = "-" + nextSlideLeft;
-  currentSlide.classList.remove("is-selected");
-  nextSlide.classList.add("is-selected");
+  updateSlide(nextSlide, currentSlide);
   if (!nextSlide.nextElementSibling) {
     nextButton.setAttribute("hidden", true);
   }
+  updateActiveDot("next");
 });
 
 prevButton.addEventListener("click", () => {
   const currentSlide = carousel.querySelector(".is-selected");
   const prevSlide = currentSlide.previousElementSibling;
-  const prevSlideLeft = getComputedStyle(prevSlide).left;
-  carousel.style.left = "-" + prevSlideLeft;
-  currentSlide.classList.remove("is-selected");
-  prevSlide.classList.add("is-selected");
+  updateSlide(prevSlide, currentSlide);
   if (prevSlide.nextElementSibling) {
     nextButton.removeAttribute("hidden");
   }
   if (!prevSlide.previousElementSibling) {
     prevButton.setAttribute("hidden", true);
   }
+  updateActiveDot("prev");
+});
+
+carouselDots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    carouselDots.forEach((dot) => {
+      dot.classList.remove("is-selected");
+    });
+    dot.classList.add("is-selected");
+    const slides = carousel.querySelectorAll(".carousel__slide");
+    const selectedSlideLeft = getComputedStyle(slides[index]).left;
+    carousel.style.left = "-" + selectedSlideLeft;
+  });
 });
